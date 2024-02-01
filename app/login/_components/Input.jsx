a288e18bb149e084e8578b9b46c0d88ae1a43968
unknown <naditawidjoherno1@gmail.com>
2024-02-01 10:36:34 +0800

    first commit

---
+++ app/login/_components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Input = () => {
+  return (
+    <div>
+      <input placeholder="NIP" />
+    </div>
+  );
+};
+
+export default Input;
---
+++ app/login/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Input from "./_components/Input";
+
+const login = () => {
+  return (
+    <div className="bg-[#030637]">
+      Login page
+      <Input />
+      nomor
+      <Input />
+      Alamat
+    </div>
+  );
+};
+
+export default login;
