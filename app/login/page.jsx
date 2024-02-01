fb80d7593ac7439d3632576abb20d64b9cbb4e44
unknown <apriliadinaa21@gmail.com>
2024-02-01 11:53:31 +0800

    create login layout

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -3,12 +3,13 @@ import Input from "./_components/Input";
 
 const login = () => {
   return (
-    <div className="bg-[#030637]">
-      Login page
-      <Input />
-      nomor
-      <Input />
-      Alamat
+    <div className="bg-[#030637] h-screen w-screen">
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="bg-white w-[300px] h-[350px] rounded-2xl" >
+          
+
+        </div>
+      </div>
     </div>
   );
 };
