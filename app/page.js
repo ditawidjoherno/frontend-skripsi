d889d53f0432536f661ddd774eaa4ee84c7f2d64
unknown <naditawidjoherno1@gmail.com>
2024-02-01 13:26:09 +0800

    p

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
-import Image from "next/image";
+import React from "react";
 
-export default function Home() {
-  return <div>das</div>;
-}
+const login = () => {
+  return (
+    <div className="bg-[#030637] h-screen w-screen">
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="bg-white w-[300px] h-[350px] rounded-2xl"></div>
+      </div>
+    </div>
+  );
+};
+
+export default login;
